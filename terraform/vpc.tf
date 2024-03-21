d72947f583be97596df1eed1e32b54bce29b8392
Yeshwanth | A Monk in Cloud ☁️ <66474973+yeshwanthlm@users.noreply.github.com>
2024-03-21 22:30:48 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,16 +1,3 @@
-locals {
-  region = "us-east-1"
-  name   = "amonkincloud-cluster"
-  vpc_cidr = "10.123.0.0/16"
-  azs      = ["us-east-1a", "us-east-1b"]
-  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
-  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
-  intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
-  tags = {
-    Example = local.name
-  }
-}
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 4.0"
