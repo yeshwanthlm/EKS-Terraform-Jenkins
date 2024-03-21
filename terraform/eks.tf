22e0e26fae24ab9cee754a800e97ea8e16561d88
Yeshwanth | A Monk in Cloud ☁️ <66474973+yeshwanthlm@users.noreply.github.com>
2024-03-21 23:23:26 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -30,7 +30,7 @@ module "eks" {
   }
 
   eks_managed_node_groups = {
-    ascode-cluster-wg = {
+    amonkincloud-cluster-wg = {
       min_size     = 1
       max_size     = 2
       desired_size = 1
